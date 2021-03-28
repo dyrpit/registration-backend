9dcf640eb1871b2b98f3e42a94871dc7fc1ef4a0
Piotr Dyrda <piotr.dyrda88@gmail.com>
2021-03-28 21:44:21 +0200

    fix: change origin

--- src/main.ts
+++ src/main.ts
@@ -2,16 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 
-const mode =
-  process.env.NODE_ENV === 'production' ? process.env.NODE_ENV : 'development';
+// const mode =
+//   process.env.NODE_ENV === 'production' ? process.env.NODE_ENV : 'development';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const origin =
-    mode === 'development' ? 'http://localhost:3001' : process.env.ORIGIN;
+  // const origin =
+  //   mode === 'development' ? 'http://localhost:3001' : process.env.ORIGIN;
 
-  app.enableCors({ origin, credentials: true });
+  app.enableCors({ origin: process.env.ORIGIN, credentials: true });
   app.use(cookieParser());
 
   await app.listen(process.env.PORT || 3000);
